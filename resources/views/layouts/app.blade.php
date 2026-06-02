35f47d81fe21057c7a73b11a0ae7684c5018c65e
Atashia <atashiaalmo5@gmail.com>
2026-06-02 22:48:39 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
 </head>
 <body>
     @include('partials.navbar')
